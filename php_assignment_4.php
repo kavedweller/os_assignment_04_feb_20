4e839dea3f5f6294dbd1434be26f7732e1ed7a25
A Kabir Chowdhury <anisulkabir.c@gmail.com>
2023-02-20 21:10:11 +0600

    added: solution 3

--- php_assignment_4.php
+++ php_assignment_4.php
@@ -31,4 +31,16 @@ $strB = "bar";
 
 echo "\nconcatenate ".$strA." and ".$strB.": ";
 echo concat($strA, $strB);
-echo "\r\n";
+echo "\r\n\n";
+
+// 3. function to remove the first and last element from an array
+
+function stripTerminalElements($array)
+{
+    return array_slice(array_slice($array,1),0,-1);
+}
+
+$list = array("e0", "e1","e2", "e3", "e4", "e5", "e6");  
+
+print_r(stripTerminalElements($list));
+echo "\r\n\n";
