c56abf0e5916450afcdbe9eae5379c91f7889bd7
A Kabir Chowdhury <anisulkabir.c@gmail.com>
2023-02-20 21:07:12 +0600

    added: solution 2

--- php_assignment_4.php
+++ php_assignment_4.php
@@ -18,3 +18,17 @@ $a = array("Volvo","Mercedes", "BMW", "Toyota");
 print_r($a);
 echo "sorted-";
 print_r(sortByStringLength($a));
+
+// 2. concatenate two strings
+
+function concat($str1, $str2)
+{
+    return $str1.$str2;
+}
+
+$strA = "foo";
+$strB = "bar";
+
+echo "\nconcatenate ".$strA." and ".$strB.": ";
+echo concat($strA, $strB);
+echo "\r\n";
